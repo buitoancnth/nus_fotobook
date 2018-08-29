b4544ca8b735e01a5c5e4c243c2f19d6a5dff5d9
Bui Toan <buitoanncth@gmail.com>
2018-08-29 21:14:58 +0700

    added jQuery validation for form signup-signin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,51 @@
 //= require rails-ujs
 //= require turbolinks
 //= require bootstrap
+//= require jquery.validate
+//= require jquery.validate.additional-methods
 //= require_tree .
+
+$("#new_user").validate({
+  errorPlacement: function (error, element) {
+    error.insertBefore(element);
+  },
+  rules: {
+    "user[first_name]": {
+      required: true,
+      maxlength: 25
+    },
+     "user[last_name]": {
+      required: true,
+      maxlength: 25
+    },
+    "user[email]": {
+      required: true,
+      email: true,
+      maxlength: 255,
+      uniqueness: true
+    },
+    "user[password]": {
+      required: true,
+      maxlength: 64
+    }
+  }, messages: {
+    "user[first_name]": {
+      required: "is required",
+      maxlength: "must less than 25 characters"
+    },
+     "user[last_name]": {
+      required: "is required",
+      maxlength: "must less than 25 characters"
+    },
+    "user[email]": {
+      required: "is required",
+      email: "Please enter a valid email address",
+      maxlength: "must less than 255 characters",
+      uniqueness: "must be unique"
+    },
+    "user[password]": {
+      required: "is required",
+      maxlength: "must less than 64 characters"
+    }
+  }
+});
