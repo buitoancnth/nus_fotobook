0804a391924402a52f6c9ba21c0729a6c524b3e0
Bui Toan <buitoanncth@gmail.com>
2018-09-07 17:52:41 +0700

    upload image with preview

--- app/assets/javascripts/image_onclick.js
+++ app/assets/javascripts/image_onclick.js
@@ -8,5 +8,28 @@ $(document).on('turbolinks:load', function(){
       });
 
       $('ul.nav>li>a[href="'+pathname+'"]').parent().addClass('active');
+      $('#click_id').on('click', function () {
+        $('#photo_image').trigger('click');
+      });
+      $('#click_id').on('change', function (e) {
+        for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+          var file = e.originalEvent.srcElement.files[i];
+          var img = document.createElement("img");
+          var reader = new FileReader();
+          reader.onloadend = function() {
+            $('.image-upload-wrap').hide();
+            $('.file-upload-image').attr('src', reader.result);
+            $('.file-upload-content').show();
+            $('.image-title').html(file.name);
+          };
+          reader.readAsDataURL(file);
+        }
+      });
+      $('#remove-image').on('click', function(){
+        $('#photo_image').val("");
+        $('#click_id').val("");
+        $('.file-upload-content').hide();
+        $('.image-upload-wrap').show();
+      });
   });
 });
