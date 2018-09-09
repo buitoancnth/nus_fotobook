50f4806ef0357c0573fef863891387dcacc42c74
Bui Toan <buitoanncth@gmail.com>
2018-09-09 09:58:24 +0700

    fix photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :album, optional: true
-  has_attached_file :image, styles: { thumb: ["100x100#", :png] }
+  has_attached_file :image, styles: { thumb: ["200x150#", :png] }
   validates_attachment :image, content_type: { content_type: /\Aimage\/.*\z/ }, presence: true
   scope :order_by_created_at, -> { order(created_at: :desc) }
   scope :photo_pucblic, -> { where(share_mode: true) }
